78c9afbf3a9d7edb1f8350d236d7c46cf612ab47
Zulqarnain <zulqarnainchohan250@gmail.com>
2025-06-21 02:39:04 -0700

    added route in backend

--- server/app.js
+++ server/app.js
@@ -8,7 +8,8 @@ const cartRoute = require("./routes/cartRoute")
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    // origin: ["http://localhost:5173"],
+    origin: ["https://wolf-sports-shahdara.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
